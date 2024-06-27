bd5508a2dd9e1d3235703024d06e13a88c43a8db
Alkly <106326904+thealkly@users.noreply.github.com>
2024-06-27 21:31:42 +0200

    Update sonoff-ota-flash.sh for sonoff R2 Mini
    added support for finding out the Device ID

--- sonoff-ota-flash.sh
+++ sonoff-ota-flash.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #
-# BASH Shell script to flash a Sonoff DIY module Over The Air.
-# 
+# BASH Shell script to flash a Sonoff MINI with Tasmota Over The Air.
+#
 
-FIRMWARE_URL_BASE="http://sonoff-ota.aelius.com/"
-DEFAULT_FILENAME="tasmota-latest-lite.bin"
+FIRMWARE_URL="http://sonoff-ota.aelius.com/tasmota-latest-lite.bin"
+SHA256SUM="5c1aecd2a19a49ae1bec0c863f69b83ef40812145c8392eebe5fd2677a6250cc"
 IPADDRESS=
-SHASUM=
+DEVICEID=
 
 # JSON Pretty Print by Evgeny Karpov
 # https://stackoverflow.com/a/38607019/1156096
@@ -16,16 +16,17 @@ json_pretty_print() {
 }
 
 sonoff_http_request() {
-  local hostname="${1}"
-  local path="${2}"
-  local body="${3:-}"
-  local url="http://${hostname}:8081/zeroconf/${path}"
+  local path="${1}"
+  local body="${2:-}"
+  local url="http://${IPADDRESS}:8081/zeroconf/${path}"
 
   if [ -z "${body}" ]; then
-    body='{"deviceid":"","data":{}}'
+    body="{\"deviceid\":\"${DEVICEID}\",\"data\":{}}"
   fi
 
-  # Build up the curl command arguments as an array
+  echo "Sending request to: ${url}"
+  echo "Request body: ${body}"
+
   cmd=('curl' '--silent' '--show-error')
   cmd+=('-XPOST')
   cmd+=('--header' "Content-Type: application/json")
@@ -41,14 +42,13 @@ sonoff_http_request() {
     exit $exit_code
   fi
 
+  echo "Response: ${output}"
   echo "${output}" | json_pretty_print
   sleep 1
 }
 
-
 lookup_ip_address() {
   local hostname="${1}"
-  # We use dscacheutil / getent because it can do multicast dns lookups
   if command -v dscacheutil &> /dev/null; then
     dscacheutil -q host -a name "${hostname}" | grep -m 1 'ip_address:' | awk '{print $2}'
   elif command -v getent &> /dev/null; then
@@ -61,10 +61,8 @@ lookup_ip_address() {
 
 mdns_browse() {
   local service="${1}"
-  local domain="${2:local.}"
+  local domain="${2:-local.}"
   if command -v dns-sd &> /dev/null; then
-    # Use the 'dns-sd' command on Mac OS
-    # expect is used because dns-sd doesn't timeout
     output=$(expect <<-EOD
 			set timeout 10
 			spawn -noecho dns-sd -B ${service} ${domain}
@@ -76,13 +74,11 @@ mdns_browse() {
 			}
 		EOD
     )
-    # Find the first 'Add' line from the output
     echo "${output}" | grep -m 1 '  Add  ' | awk '{sub("\r", "", $NF); print $NF}'
   elif command -v avahi-browse &> /dev/null; then
-    # Use the 'avahi-browse' command on Linux
     avahi-browse -pt -d "${domain}" "${service}" | awk 'BEGIN {FS=";"} {if ($1=="+" && $3=="IPv4") print $4}'
   else
-    echo "Unable to perform multicast DNS discovery : didn't find dns-sd or avahi-browse." >&2
+    echo "Unable to perform multicast DNS discovery: didn't find dns-sd or avahi-browse." >&2
     exit 1
   fi
 }
@@ -98,36 +94,35 @@ discover_module() {
     echo "Hostname: ${hostname}"
   fi
 
-  # Now get the IP address for the hostname
+  DEVICEID=$(echo "${hostname}" | grep -o '100[0-9a-fA-F]\+')
   IPADDRESS=$(lookup_ip_address "${hostname}.local.")
   if [ -z "${IPADDRESS}" ]; then
     echo "Failed to resolve IP address for ${hostname}" >&2
     exit 3
   fi
   echo "IPv4 Address: ${IPADDRESS}"
+  echo "Device ID: ${DEVICEID}"
   echo
 }
-  
+
 display_info() {
   echo "Getting Module Info..."
-  sonoff_http_request "${IPADDRESS}" info
+  sonoff_http_request "info"
   echo
 }
 
 ota_unlock() {
   echo "Unlocking for OTA flashing..."
-  # FIXME: skip this if already unlocked
-  sonoff_http_request "${IPADDRESS}" ota_unlock
+  sonoff_http_request "ota_unlock" "{\"deviceid\":\"${DEVICEID}\",\"data\":{}}"
   echo
 }
 
 ota_flash() {
   read -p "Proceed with flashing? [N/y] " -n 1 -r
-  echo    # (optional) move to a new line
-  if [[ $REPLY =~ ^[Yy]$ ]]
-  then
+  echo
+  if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Requesting OTA flashing..."
-    sonoff_http_request "${IPADDRESS}" ota_flash "{\"deviceid\":\"\",\"data\":{\"downloadUrl\":\"${FIRMWARE_URL}\",\"sha256sum\":\"${SHASUM}\"}}"
+    sonoff_http_request "ota_flash" "{\"deviceid\":\"${DEVICEID}\",\"data\":{\"downloadUrl\":\"${FIRMWARE_URL}\",\"sha256sum\":\"${SHA256SUM}\"}}"
     echo
   else
     echo "Aborting"
@@ -152,110 +147,8 @@ check_firmware_exists() {
   fi
 }
 
-lookup_shasum() {
-  echo "Looking up sha256sum for firmware"
-  output=$(curl '--fail' '--silent' '--show-error' "${FIRMWARE_URL}.sha256")
-  exit_code=$?
-  if [ "$exit_code" -ne 0 ]; then
-    echo "Failed to get .sha256 file for: ${FIRMWARE_URL}" >&2
-    echo "Please add a .sha256 file to server, or pass the SHA256 on the command line" >&2
-    exit $exit_code
-  fi
-
-  # Check it looks like it is in the right format
-  if [[ $output =~ ^([0-9a-f]{64})\ \*(.+)$ ]]; then
-    SHASUM="${BASH_REMATCH[1]}"
-    echo "OK"
-    echo
-  else
-    echo "Failed to parse SHA256 sum from file: ${output}" >&2
-    exit 2
-  fi
-}
-
-display_help() {
-  printf "Usage: %s [options] [<filename or url>]\n" "${0}"
-  printf "If just a filename is given, it is relative to %s\n\n" "${FIRMWARE_URL_BASE}"
-  printf -- "Options:\n"
-  grep -E -e '^[[:space:]]*# PARAM_Usage:' -e '^[[:space:]]*# PARAM_Description:' "${0}" | while read -r usage; read -r description; do
-    if [[ ! "${usage}" =~ Usage ]] || [[ ! "${description}" =~ Description ]]; then
-      _exiterr "Error generating help text."
-    fi
-    printf " %-32s %s\n" "${usage##"# PARAM_Usage: "}" "${description##"# PARAM_Description: "}"
-  done
-
-  exit 1
-}
-
-
-parse_options() {
-  check_parameters() {
-    if [[ -z "${1:-}" ]]; then
-      echo "The specified command requires additional parameters. See help:" >&2
-      display_help >&2
-      exit 1
-    elif [[ "${1:0:1}" = "-" ]]; then
-      _exiterr "Invalid argument: ${1}"
-    fi
-  }
-
-  local params=()
-  while (( "$#" )); do
-    case "$1" in
-      # PARAM_Usage: -i, --ipaddress <ipaddress>
-      # PARAM_Description: Specify the IP address of the Sonoff module
-      -i|--ipaddress)
-        shift 1
-        check_parameters "${1:-}"
-        IPADDRESS="${1}"
-        ;;
-      # PARAM_Usage: -s, --sha256 <sha256>
-      # PARAM_Description: Specify the SHA256 sum of the firmware
-      -s|--sha256)
-        shift 1
-        check_parameters "${1:-}"
-        SHASUM="${1}"
-        ;;
-      # PARAM_Usage: -h, --help
-      # PARAM_Description: Display this message
-      -h|--help)
-        display_help
-        ;;
-      -*)
-        echo "Error: Unsupported flag $1" >&2
-        display_help
-        ;;
-      *) # preserve positional arguments
-        if [ -n "$1" ]; then
-          params+=("$1")
-        fi
-        ;;
-    esac
-    shift
-  done
-
-  if [ "${#params[@]}" -eq 0 ]; then
-    FIRMWARE_URL="${DEFAULT_FILENAME}"
-  elif [ "${#params[@]}" -eq 1 ]; then
-    FIRMWARE_URL="${params[0]}"
-  else
-    display_help
-  fi
-
-  # If the filename doesn't start http: then prepend the base URL  
-  if [[ ! ${FIRMWARE_URL} =~ ^https?: ]]; then
-    FIRMWARE_URL="${FIRMWARE_URL_BASE}${FIRMWARE_URL}"
-  fi
-}
-
 main() {
-  parse_options "${@:-}"
-
   check_firmware_exists
-
-  if [ -z "${SHASUM:-}" ]; then
-    lookup_shasum
-  fi
 
   if [ -z "${IPADDRESS:-}" ]; then
     discover_module
@@ -265,12 +158,11 @@ main() {
 
   ota_unlock
 
+  display_info
+
   ota_flash
   
   echo "Please wait for your device to finish flashing."
 }
 
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
-then
-  main "${@:-}"
-fi
+main
